9a6f134c408498ceae7cad9291d03ebbfc935e5f
Firly Anastasya Hafid <firlyanastasyahafid@MacBook-Air-Firly.local>
2026-01-22 15:37:05 +0800

    Update konfigurasi database

--- app/models/config/config.php
+++ app/models/config/config.php
@@ -1,6 +1,6 @@
 <?php
 // URL Dasar (Sesuaikan dengan folder langkah 1)
-define('BASE_URL', 'http://localhost/Code/Project_ICLabs/iclabs_v2/public/');
+define('BASE_URL', 'http://localhost:8080/iclabs_v2/public/');
 
 // Database
 define('DB_HOST', 'localhost');
