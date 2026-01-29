75d8300776600e5474d39856b74e97db4aa8ad44
FIRLI ANASTASYA HAFID <firlyanastasya512@gmail.com>
2026-01-29 15:36:06 +0800

    feat : fix link

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,7 @@
 <?php
 // URL Dasar (Sesuaikan dengan folder langkah 1)
 // define('BASE_URL', 'http://0.0.0.0/Code/Project_ICLabs/iclabs_v2/public/');
-define('BASE_URL', 'http://localhost:8080/iclabs_v2/public/');
+define('BASE_URL', 'http://localhost/Monitoring-Asisten/public/');
 
 // Database
 define('DB_HOST', 'localhost');
